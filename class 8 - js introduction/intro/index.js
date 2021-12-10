c864b2ca496ab5767dbf230c26159f95a1957891
Juan Duque <hanzduque393@gmail.com>
2021-12-09 21:10:18 -0500

    js introduction files added

---
+++ class 8 - js introduction/intro/index.js
@@ -0,0 +1,60 @@
+var numero = 100;
+let number = 1000;
+const otroNumero = 200
+numero = 600
+number = 500
+//otroNumero = 2000
+console.log("Numeros: ", numero, number, otroNumero);
+
+let boolean = true | false;//This is a boolean
+let number1 = 10; //This is a number
+let string = "Esto es un string" | 'This is a string'; //This is a string
+//const otherString = ´This is a string
+
+
+if(2 === "2"){
+  console.log("true");
+}
+
+const num = 19;
+if( num < 18 ){
+  console.log("si es mayor")
+} else{
+  console.log("no es mayor")
+}
+
+if( true ){
+  console.log("si es mayor")
+} else{
+  console.log("no es mayor")
+}
+
+let day = 10;
+let month = 5;
+/*
+if( day >= 1 && day >= 31 ){
+  if(){
+
+  }if (condition) {
+    
+  } if (condition)
+  
+  else {
+    
+  }
+}else{
+  console.log("Ingrese un día válido")
+}
+*/
+/*
+for (let index = 0; index <= 5; index++) {
+  console.log(index)  
+}
+*/
+
+
+let number2 = 0;
+while(number2 < 5){
+  console.log("hola");
+  number2++
+}
