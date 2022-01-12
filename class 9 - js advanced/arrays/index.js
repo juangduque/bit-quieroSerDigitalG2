de4f92b70937317fb3f089a718d5e18e1dc74b2f
Juan Duque <hanzduque393@gmail.com>
2022-01-11 19:48:11 -0500

    arrays functions updated

--- class 9 - js advanced/arrays/index.js
+++ class 9 - js advanced/arrays/index.js
@@ -1,11 +1,11 @@
-/*
+
 let area;
 const pi = Math.PI
 
 function calculateArea(circleZise){
   return pi * Math.pow(circleZise, 2);
 };
-*/
+
 let array = [1,2,3,4,5];
 array.push(6);
 let array1 = ["hola", 2, true];
@@ -19,3 +19,25 @@ function readArray(arrayReaded){
 
 readArray(array);
 readArray(array1);
+
+function readArray(arrayReaded){
+  for (let index = 0; index < arrayReaded.length; index++) {
+    console.log(arrayReaded[index]);  
+  };
+};
+
+let fruits = ['Banana', 'apple', 'kiwi'];
+fruits.push('pitaya');
+fruits.forEach( function(fruit){
+  console.log(fruit);
+})
+fruits.shift()
+
+const numbers = [1,2,3,4,5];
+const newNumbersArray = numbers.map( 
+  function(x){
+    console.log("this is the entried number: ", x)
+    return x * 2;
+  }
+);
+console.log(newNumbersArray)
