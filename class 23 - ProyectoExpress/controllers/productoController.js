38314596fd0839404dc99880e679907185fafb2d
Miguel Nieto <migustanieto.2@gmail.com>
2022-02-15 19:51:43 -0500

    proceso para CRUD express mongo

--- class 23 - ProyectoExpress/controllers/productoController.js
+++ class 23 - ProyectoExpress/controllers/productoController.js
@@ -1,6 +1,19 @@
 const Producto = require('../models/Producto');
 
-exports.crearProducto = (req, res) => {
-    console.log('Creando el producto desde el controlador');
-    console.log(req.body);
+exports.crearProducto = async(req, res) => {
+    // console.log('Creando el producto desde el controlador');
+    // console.log(req.body);
+
+    try {
+
+        let data_producto;
+        data_producto = new Producto(req.body);
+        await data_producto.save();
+        res.send(data_producto)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ups... Hay un error, comuniquese con soporte');
+    }
+
 }
