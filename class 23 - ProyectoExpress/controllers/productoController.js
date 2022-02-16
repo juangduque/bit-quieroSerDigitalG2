d4979504a96619c52a41849bf8e56e8edf02d921
Miguel Nieto <migustanieto.2@gmail.com>
2022-02-15 21:35:15 -0500

    proceso para CRUD express mongo

--- class 23 - ProyectoExpress/controllers/productoController.js
+++ class 23 - ProyectoExpress/controllers/productoController.js
@@ -5,7 +5,6 @@ exports.crearProducto = async(req, res) => {
     // console.log(req.body);
 
     try {
-
         let data_producto;
         data_producto = new Producto(req.body);
         await data_producto.save();
@@ -16,4 +15,67 @@ exports.crearProducto = async(req, res) => {
         res.status(500).send('Ups... Hay un error, comuniquese con soporte');
     }
 
+}
+
+exports.consultarProductos = async(req, res) => {
+    try {
+        const data_producto = await Producto.find();
+        res.json(data_producto);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ups... Hay un error, comuniquese con soporte');
+    }
+}
+
+exports.consultarProducto = async(req, res) => {
+    try {
+        const data_producto = await Producto.findById(req.params.id);
+
+        if (!data_producto) {
+            res.status(404).json({ mensaje: 'No se encontraron coincidencias' })
+        }
+        res.json(data_producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ups... Hay un error, comuniquese con soporte');
+    }
+}
+
+exports.actualizarProducto = async(req, res) => {
+    try {
+        const { nombre, precio, proveedor, cantidad_stock } = req.body
+        let data_producto = await Producto.findById(req.params.id);
+
+        if (!data_producto) {
+            res.status(404).json({ mensaje: 'No se encontraron coincidencias para la actualización de datos' })
+        }
+
+        data_producto.nombre = nombre;
+        data_producto.precio = precio;
+        data_producto.proveedor = proveedor;
+        data_producto.cantidad_stock = cantidad_stock;
+
+        data_producto = await Producto.findOneAndUpdate({ _id: req.params.id }, data_producto, { new: true })
+        res.json(data_producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ups... Hay un error, comuniquese con soporte');
+    }
+}
+
+
+exports.eliminarProducto = async(req, res) => {
+    try {
+        const data_producto = await Producto.findById(req.params.id);
+        if (!data_producto) {
+            res.status(404).json({ mensaje: 'No se encontraron coincidencias para eliminar productos' })
+        }
+        await Producto.findByIdAndRemove({ _id: req.params.id });
+        res.json({ mensaje: 'Producto eliminado correctamente' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ups... Hay un error, comuniquese con soporte');
+    }
 }
--- class 23 - ProyectoExpress/routes/producto.js
+++ class 23 - ProyectoExpress/routes/producto.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const productoController = require('../controllers/productoController');
 
 router.post('/', productoController.crearProducto);
+router.get('/', productoController.consultarProductos);
+router.get('/:id', productoController.consultarProducto);
+router.put('/:id', productoController.actualizarProducto);
+router.delete('/:id', productoController.eliminarProducto);
 
 module.exports = router;
