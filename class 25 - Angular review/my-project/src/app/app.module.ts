6b6f383f606c5921fa41aa96b48295f95435ccd9
Juan Duque <juangduqued@gmail.com>
2022-03-03 21:33:12 -0500

    upload Angular content

--- class 25 - Angular review/my-project/src/app/app.module.ts
+++ class 25 - Angular review/my-project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -16,7 +17,8 @@ import { AsideComponent } from './components/aside/aside.component';
     AsideComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
