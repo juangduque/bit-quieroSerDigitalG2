9b5d29866d26ce06fe2c587696091ff562b9a8c2
Juan Duque <juangduqued@gmail.com>
2022-03-03 19:35:20 -0500

    upload components

--- class 25 - Angular review/my-project/src/app/app.component.ts
+++ class 25 - Angular review/my-project/src/app/app.component.ts
@@ -6,28 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  text = 'Hello World!';
-  number = 5;
-  source = 'https://www.w3schools.com/howto/img_avatar.png';
-  parrafo = "vgfgsgsdsdrrtbs";
-  otraCosa = "esto es una entrada";
-  elementType = "text";
-  person = {
-    name: 'Nicolas',
-    age: 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-  }
 
-  sayHello(){
-    alert("Hola mundo desde una función");
-  }
-
-  changeName(){
-    this.person.name = "Juan";
-  }
-
-  changeNameByInput(event: Event){
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
 }
--- class 25 - Angular review/my-project/src/app/app.module.ts
+++ class 25 - Angular review/my-project/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AsideComponent,
   ],
   imports: [
     BrowserModule
