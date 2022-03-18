eb21b19d62e4da23417d9f9aae6cf14e9d5cac74
Juan Duque <juangduqued@gmail.com>
2022-03-17 21:21:22 -0500

    get and post method implemented

--- class 29 - express review/index.js
+++ class 29 - express review/index.js
@@ -1,9 +1,88 @@
 const express = require("express");
+const body_parser = require("body-parser");
 //http://localhost: o http://127.0.0.1:
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(body_parser.json());
+app.use(express.json());
+
+let users = [
+  {
+    id: "1",
+    name: "Jhon",
+    last_name: "Doe",
+    id_number: "1234",
+    user_name: "jhon123",
+    email: "jdoe@mail.com",
+    password: "abc123"
+  },
+  {
+    id: "2",
+    name: "Oscar",
+    last_name: "Diaz",
+    id_number: "5678",
+    user_name: "oscar123",
+    email: "oscar@mail.com",
+    password: "qwer123"
+  },
+  {
+    id: "3",
+    name: "Ana",
+    last_name: "Smith",
+    id_number: "1278",
+    user_name: "ana123",
+    email: "ana@mail.com",
+    password: "bnm567"
+  },
+];
+
+app.get('/users', (req, res) => {
+  if(users){
+    res.json(users);
+  }else{
+    res.send("Hubo un error");
+    console.error(new Error("No hay usuarios en la base de datos") );
+  };
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  // for (let index = 0; index < users.length; index++) {
+  //   if(users[index].id === id){
+  //     res.json(users[index]);
+  //   }
+  // };
+
+  users.forEach((item) => {
+    if(item.id === id){
+      res.json(item);
+    }
+  });
+
+  // const userToBeReturned = users.filter(user => user.id === id);
+  // if(userToBeReturned !== []){
+  //   res.json(userToBeReturned)
+  // };
+  res.json("Usuario no existe");
+});
+
+app.post('/users', function(req, res){
+  // const { body } = req;
+  console.log("body:", req.body)
+  res.json("Usuario creado exitosamente");
+});
+
+app.put('/users/:id', function(req, res){
+  res.json("Hola desde PUT");
+});
+
+app.delete('/users/:id', function(req, res){
+  res.json("Hola desde DELETE");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
--- class 29 - express review/readme.md
+++ class 29 - express review/readme.md
@@ -12,3 +12,36 @@ npm install --save express
 ```
 npm install -D nodemon
 ```
+
+##### Ejemplo concatenación de queries:
+Con & concatenamos los queries
+http://127.0.0.1:3000/users/cualquierCosa?otroNombre=20&MiNombre=Juan
+
+
+##### Métodos http requeridos por cada entidad
+```
+app.get('/users', (req, res) => {
+  res.json("Hola desde GET");
+});
+
+app.get('/users/:id', (req, res) => {
+  res.json("Hola desde GET");
+});
+
+app.post('/users', function(req, res){
+  res.json("Hola desde POST");
+});
+
+app.put('/users/:id', function(req, res){
+  res.json("Hola desde PUT");
+});
+
+app.delete('/users/:id', function(req, res){
+  res.json("Hola desde DELETE");
+});
+```
+##### Instalar la librería body parser
+```
+npm i -S body-parser
+```
+Esta librería es necesaria para usar los datos del body en nuestra petición http.
